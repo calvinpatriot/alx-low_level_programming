4f9b1f074174bad3dd19f3697cef0651a085f2fb
calvinpatriot <calvinpatriot@gmail.com>
2023-05-08 12:21:44 -0700

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 
 /**
  * create_file - Creates a file.
- * @filename:  pointer to the name of the file o be  created.
+ * @filename:  pointer to the name of the file to be  created.
  * @text_content:  pointer to the  string to write to the file.
  *
  * Return: If the function fails - -1.
@@ -10,17 +10,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
+	int fd, w, len = 0;
 	
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len =0; text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
-	fd = open(filename, O_CREAT |O_RDWR |O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 	if (fd == -1 || w == -1)
 		return (-1);
